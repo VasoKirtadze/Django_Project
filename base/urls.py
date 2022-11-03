50ba4c755226c6d1cf896c761fbded6376ba7a5a
Vaso Kirtadze <qvseknela@gmail.com>
2022-11-03 20:33:37 +0400

    Added styling to most of the pages

--- base/urls.py
+++ base/urls.py
@@ -2,11 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login_page, name='login'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('profile/<str:pk>/', views.user_profile, name='user_profile'),
+    path('update/<str:pk>/', views.update_user, name='update_user'),
 
     path('', views.home, name='home'),
     path('room/<str:pk>', views.room, name='room'),
     path('create_room/', views.create_room, name='create_room'),
     path('update_room/<str:pk>', views.update_room, name='update_room'),
     path('delete_room/<str:pk>', views.delete_room, name='delete_room'),
+    path('delete_message/<str:pk>', views.delete_message, name='delete_message'),
 ]

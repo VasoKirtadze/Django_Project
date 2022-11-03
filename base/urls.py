8253aa4e2b52fc42255b18f2a6dc4b3149923cda
Vaso Kirtadze <qvseknela@gmail.com>
2022-11-03 12:53:36 +0400

    Started working on Django project

---
+++ base/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login', views.login_page, name='login'),
+
+    path('', views.home, name='home'),
+    path('room/<str:pk>', views.room, name='room'),
+    path('create_room/', views.create_room, name='create_room'),
+    path('update_room/<str:pk>', views.update_room, name='update_room'),
+    path('delete_room/<str:pk>', views.delete_room, name='delete_room'),
+]
